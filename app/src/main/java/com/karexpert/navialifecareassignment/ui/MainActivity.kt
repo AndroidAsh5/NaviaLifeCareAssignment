e723e07e5947987026610791308b5451d933c907
AndroidAsh5 <ashmeetarora68@yahoo.com>
2021-03-04 22:22:51 +0530

    CC

--- app/src/main/java/com/karexpert/navialifecareassignment/ui/MainActivity.kt
+++ app/src/main/java/com/karexpert/navialifecareassignment/ui/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        
+
         setupUI()
         setupViewModel()
         setupObservers()
